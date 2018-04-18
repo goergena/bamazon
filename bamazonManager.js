a41224d49bdf570729f90b33a466624239f3c570
goergena <goergena@users.noreply.github.com>
2018-04-18 12:18:05 -0400

    Manager tool works. supervisor tool nextt

--- bamazonManager.js
+++ bamazonManager.js
@@ -7,18 +7,28 @@ var connection = mysql.createConnection({
 
     user: 'root',
     password: 'root',
-    database: 'bamazon_db'
+    resbase: 'bamazon_db'
 });
 
 var choicesArray = ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'];
 
+inquirer.prompt([{
+    name: 'action',
+    type: 'list',
+    message: 'Welcome to the Management Tool. Please select an action from the menu:',
+    choices: choicesArray
+}]).then(function (answer) {
+    methods[answer.action]();
+});
+
+
 var methods = {
     'View Products for Sale': function () {
-        connection.query("SELECT * FROM products", function (err, data) {
+        connection.query("SELECT * FROM products", function (err, res) {
             if (err) throw err;
 
-            for (var prodIndex = 0; prodIndex < data.length; prodIndex++) {
-                console.log("ID: " + data[prodIndex].item_id + " |  " + data[prodIndex].product_name + " |  $" + data[prodIndex].price + ".00  | In Stock:  " + data[prodIndex].stock_quantity + "\n");
+            for (var prodIndex = 0; prodIndex < res.length; prodIndex++) {
+                console.log("ID: " + res[prodIndex].item_id + " |  " + res[prodIndex].product_name + " |  $" + res[prodIndex].price + ".00  | In Stock:  " + res[prodIndex].stock_quantity + "\n");
             }
             connection.end();
         });
@@ -27,16 +37,21 @@ var methods = {
         connection.query("SELECT * FROM products WHERE stock_quantity <5", function (err, res) {
             if (err) throw err;
             // Log all results of the SELECT statement
-            console.log(res);
+            if (!res === []) {
+                console.log(res);
+            } else {
+                console.log("There are no products where inventory is below 5.")
+            }
+
             connection.end();
         });
     },
     'Add to Inventory': function () {
-        connection.query("SELECT * FROM products", function (err, data) {
+        connection.query("SELECT * FROM products", function (err, res) {
             if (err) throw err;
             var productArray = [];
-            for (var prodIndex = 0; prodIndex < data.length; prodIndex++) {
-                productArray.push(data[prodIndex].product_name);
+            for (var prodIndex = 0; prodIndex < res.length; prodIndex++) {
+                productArray.push(res[prodIndex].product_name);
             }
 
             inquirer.prompt([{
@@ -52,35 +67,59 @@ var methods = {
                         var value = parseInt(value);
                         return !isNaN(value);
                     }
-
                 }
             ]).then(function (answer) {
                 connection.query("SELECT stock_quantity FROM products WHERE ?", [{
                     product_name: answer.restock
-                }], function(err, data) {
-                    console.log(data[0].stock_quantity);
-                    var newStock = parseInt(answer.addToStock) + parseInt(data[0].stock_quantity);
+                }], function (err, res) {
+                    var newStock = parseInt(answer.addToStock) + parseInt(res[0].stock_quantity);
                     updateStock(answer.restock, newStock);
                 });
             });
         });
     },
+    'Add New Product': function () {
+        inquirer.prompt([{
+                name: 'name',
+                message: 'Enter the name of the new product:',
+            },
+            {
+                name: 'department',
+                message: 'What department is this product in?:',
+            },
+            {
+                name: 'price',
+                message: 'What is the price of this product?',
+                validate: function (value) {
+                    var value = parseInt(value);
+                    return !isNaN(value);
+                }
+            },
+            {
+                name: 'stock',
+                message: 'How many would you like to stock?',
+                validate: function (value) {
+                    var value = parseInt(value);
+                    return !isNaN(value);
+                }
+            }
+        ]).then(function (answer) {
+
+            connection.query(
+                "INSERT INTO products SET ?", {
+                    product_name: answer.name,
+                    department_name: answer.department,
+                    price: parseInt(answer.price),
+                    stock_quantity: parseInt(answer.stock)
+                },
+                function (err, res) {
+                    console.log(res.affectedRows + " product added!\n");
+                    connection.end();
+                }
+            );
+        });
+    }
 };
-
-inquirer.prompt([{
-    name: 'action',
-    type: 'list',
-    message: 'Welcome to the Management Tool. Please select an action from the menu:',
-    choices: choicesArray
-}]).then(function (answer) {
-    var call = answer.action;
-    // console.log(answer);
-    // console.log(answer.action);
-    methods[call]();
-    // methods['View Products for Sale']();
-
-});
-
 
 function updateStock(name, quant) {
     connection.query(
@@ -91,7 +130,7 @@ function updateStock(name, quant) {
                 product_name: name
             }
         ],
-        function (err, data) {
+        function (err, res) {
             if (err) throw err;
             console.log("Restock successful!");
             connection.end();
